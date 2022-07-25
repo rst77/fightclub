30ad982fbc23203158ab0deaa3e3e1a44674b634
Renato Teixeira <rst77>
2022-07-25 18:55:30 +0000

    Initial commit

---
+++ src/main/java/com/r13a/fightclub/Application.java
@@ -0,0 +1,9 @@
+package com.r13a.fightclub;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
